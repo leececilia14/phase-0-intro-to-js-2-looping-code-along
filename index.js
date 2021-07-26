32feb1e06e399c3ca983d49f3ca26f968a708c0e
cecilia <leececilia14@gmail.com>
2021-07-26 15:44:08 +0900

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
+
+const gifts= ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+  }
+  return gifts;
+}
+
+
 function writeCards( namesArray, event ) {
-    let thankYouCards = []
-    for ( let i = 0; i < namesArray.length; i++ ) {
-      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-    }
-    return thankYouCards
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+
   }
-  
-  function countDown( startingNumber ) {
-    while ( startingNumber > 0 ) {
-      console.log( startingNumber );
-      startingNumber -= 1;
-    }
-    console.log( startingNumber );
-  }
+  return thankYouCards
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
+
+function countDown(number){
+  while (number > 0){
+    console.log(number);
+    number -=1
+  };
+  console.log(number);
+ 
+}
